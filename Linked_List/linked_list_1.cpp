f1870cc7ffea327bc42baf44bb5ed30527cb7fed
Sohyun An <90887813+cownow4425@users.noreply.github.com>
2021-12-20 18:26:47 +0900

    Create linked_list_1.cpp

---
+++ Linked_List/linked_list_1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  struct node{
+  char *data;
+  struct node *next; // 다음 노드의 주소를 저장할 필드이다. 이렇게 자신과 동일한 구조체에 대한 포인터를 멤버로 가진다는 의미에서 "자기참조형 구조체"라고 부르기도 한다.
+  };
+
+  typedef struct node Node;
+  Node *head = NULL; // 연결리스트의 첫 번째 노드의 주소를 저장할 포인터이다.
+}
