d3140ae54ef993e9b32c882a84c79a25ea6a24a0
cownow4425 <cownow4425@gmail.com>
2021-12-23 16:54:59 +0900

    Update tree.py

--- python_implementation/tree.py
+++ python_implementation/tree.py
@@ -96,4 +96,113 @@ class BinaryTree:
         else:
             return []
 
-# Binary Search Table(BST)
+# 이진탐색트리(Binary Search Table(BST))
+# 이진탐색트리는 모든 노드 n에 대해서 '모든 왼쪽 자식들 <= n <= 모든 오른쪽 자식들'속성이 참인 것을 말한다.
+class Node:
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, key, data):
+        if key<self.key:
+            if self.left: self.left.insert(key, data)
+            else: self.left = Node(key, data)
+        elif key>self.key:
+            if self.right: self.right.insert(key, data)
+            else: self.right = Node(key, data)
+        else:
+            raise KeyError("Key %d is already exist!" % key)
+
+    def lookup(self, key, parent=None):
+        if key<self.key:
+            if self.left: return self.left.lookup(key, self)
+            else: return None, None
+        elif key>self.key:
+            if self.right: return self.right.lookup(key, self)
+            else: return None, None
+        else:
+            return self, parent
+
+    def countChildren(self):
+        count = 0
+        if self.left: count += 1
+        if self.right: count+= 1
+        return count
+
+class BinSearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key, data):
+        if self.root: return self.root.insert(key, data)
+        else: self.root = Node(key, data)
+
+    def lookup(self, key):
+        if self.root: return self.root.lookup(key)
+        else: return None, None
+    
+    def remove(self, key):
+        node, parent = self.lookup(key)
+        if node:
+            nChildren = node.countChildren()
+            # [Case 1]: The simplest case of no children
+            if nChildren == 0:
+                # 만약 parent가 있으면 
+                # node가 왼쪽 자식인지 오른쪽 자신인지 판단하여 parent.left or parent.right를 None으로 하여
+                # Leaf Node였던 자식을 트리에서 끊어내어 없앤다.
+                if parent:
+                    if parent.left == node: parent.left = None
+                    else: parent.right = None
+                # 만약 parent가 없으면 (node는 root인 경우) self.root를 None으로 하여 빈트리로 만든다.
+                else:
+                    self.root = None
+            # [Case 2]: When the node has only one child
+            elif nChildren == 1:
+                # 하나 있는 자식이 왼쪽 자식인지 오른쪽 자식인지 판단하여 그 자식을 어떤 변수가 가리키도록 한다.
+                # 만약 parent가 있으면 node가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # 위에서 가리킨 자식을 대신 node의 자리에 넣는다.
+                if parent:
+                    if node.left: # node의 자식이 왼쪽에 위치하는 경우
+                        if parent.left == node: parent.left = node.left
+                        else: parent.right = node.left
+                    else: # node의 자식이 오른쪽에 위치하는 경우
+                        if parent.left == node: parent.left = node.right
+                        else: parent.right = node.right
+                # 만약 parent가 없으면 (node는 root인 경우) self.root에 위에서 가리킨 자식을 대신 넣는다.
+                else:
+                    if node.left: self.root = node.left
+                    else: self.root = node.right
+            # [Case 3]: When the node has both left and right children
+            else:
+                parent = node
+                successor = node.right
+                # parent가 node를 가리키도록 하고 successor는 node의 오른쪽 자식 서브트리의 root node를 가리키도록 한다.
+                # successor로부터 왼쪽 자식의 링크를 반복하여 따라감으로써 순환문이 종료할 때 successor는 node의 오른쪽 자식 서브트리의 최소값을,
+                # 그리고 parent는 그 node의 부모 node를 가리키도록 찾아낸다.
+                while successor.left:
+                    parent = successor
+                    successor = successor.left
+                # 삭제하려는 노드인 node에 방금 찾은 successor의 key와 data를 대입한다.
+                node.key = successor.key
+                node.data = successor.data
+                # 이제 successor가 parent의 왼쪽 자식인지 오른쪽 자식인지 파악하여
+                # 그에 따라 parent.left 또는 parent.right를 successor가 가지고 있던(없을 수도 있지만) 자식을 가리키도록 한다.
+                if parent.left == successor:
+                    if successor.left: parent.left = successor.left
+                    elif successor.right: successor.right
+                    else: parent.left = None
+                else:
+                    if successor.left: parent.right = successor.left
+                    elif successor.right: parent.right = successor.right
+                    else: parent.right = None
+            return True
+        else:
+            return False
+            
+                
+
+
+
